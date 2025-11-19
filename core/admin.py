6455d6e528218e6b0402a2e912143548c2b60565
Jefferson Fernandes <suportefinanceiro@rcostaadv.com.br>
2025-11-19 09:43:23 -0300

    Adicionando usuarios ao admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    
+    # Adiciona os campos personalizados na lista de visualização (colunas da tabela)
+    list_display = ['username', 'email', 'telefone', 'nome_empresa', 'is_assinante', 'is_staff']
+    
+    # Adiciona os campos na tela de edição do usuário
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações Mayacorp', {'fields': ('telefone', 'cpf', 'nome_empresa', 'is_assinante', 'data_expiracao')}),
+    )
+
+# Registra o modelo
+admin.site.register(CustomUser, CustomUserAdmin)
