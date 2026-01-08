75502e41449808db180a58a9b5691bbac467ca32
Jefferson Fernandes <suportefinanceiro@rcostaadv.com.br>
2026-01-08 12:26:22 -0300

    Implementação completa do sistema de termos com variáveis dinâmicas e editor

--- termos_fit/views.py
+++ termos_fit/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from .models import TermoTemplate, TermoAssinado
 from cadastros_fit.models import Aluno
 from django.utils import timezone
+from .forms import TermoTemplateForm
 
 def gerar_termo_aluno(request, aluno_id):
     if request.method == "POST":
@@ -21,7 +22,6 @@ def assinar_termo(request, token):
     termo = get_object_or_404(TermoAssinado, token_assinatura=token)
     
     if request.method == "POST":
-        # Lógica para salvar a imagem da assinatura (Base64)
         assinatura_data = request.POST.get('assinatura_data')
         termo.assinatura_imagem = assinatura_data
         termo.data_assinatura = timezone.now()
@@ -29,16 +29,42 @@ def assinar_termo(request, token):
         termo.save()
         return JsonResponse({'status': 'ok'})
 
-    # Preenche as variáveis do template (Ex: nome do aluno)
-    texto_final = termo.template.texto_html.replace('{{ aluno_nome }}', termo.aluno.nome)
+    # Texto original do template
+    texto_final = termo.template.texto_html
+    aluno = termo.aluno
+
+    # Mapeamento completo baseado na sua model de Aluno
+    substituicoes = {
+        '[[ALUNO_NOME]]': aluno.nome,
+        '[[ALUNO_CPF]]': aluno.cpf if aluno.cpf else "___.___.___-__",
+        '[[ALUNO_DATA_NASC]]': aluno.data_nascimento.strftime('%d/%m/%Y') if aluno.data_nascimento else "__/__/____",
+        '[[ALUNO_EMAIL]]': aluno.email,
+        '[[ALUNO_TELEFONE]]': aluno.telefone,
+        '[[ALUNO_ENDERECO]]': aluno.endereco_completo, # Usando a @property que você criou
+        '[[DATA_HOJE]]': timezone.now().strftime('%d de %B de %Y'),
+        '[[NOME_UNIDADE]]': "Mayacorp Fit", # Aqui você pode buscar de Unidade se houver relação
+    }
+
+    # Faz a troca de todas as tags
+    for tag, valor in substituicoes.items():
+        texto_final = texto_final.replace(tag, str(valor))
     
     return render(request, 'termos_fit/assinar_termo.html', {
         'termo': termo,
         'texto_contrato': texto_final
     })
-
 def termo_template_list(request):
     templates = TermoTemplate.objects.filter(ativo=True)
     return render(request, 'termos_fit/termo_template_list.html', {
         'templates': templates
     })
+
+def termo_template_create(request):
+    if request.method == "POST":
+        form = TermoTemplateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('termo_template_list')
+    else:
+        form = TermoTemplateForm()
+    return render(request, 'termos_fit/termo_template_form.html', {'form': form})
